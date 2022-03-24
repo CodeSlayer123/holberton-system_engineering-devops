51e59d69586bc779cdc80820f626c29f9e56af91
Code Slayer <3613@holbertonschool.com>
2022-03-24 13:56:56 -0500

    tasks 0-1 run locally

---
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""lists progress of employee's tasks through REST API"""
+
+if __name__ == "__main__":
+    import json
+    import sys
+    import urllib.request
+
+    user_id = sys.argv[1]
+    completed = 0
+    tasks = 0
+    title = "\t "
+    x = urllib.request.urlopen('https://jsonplaceholder.typicode.com/todos/')
+    todo = json.loads(x.read())
+    for i in todo:
+        if i.get('userId') == int(user_id):
+            tasks += 1
+            if i.get('completed') == True:
+                title = title + i['title'] + '\n' + '\t '
+                completed += 1
+    x.close()
+
+
+    y = urllib.request.urlopen('https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
+    user = json.loads(y.read())
+    print("Employee {} is done with tasks ({}/{}):".format(user['name'], completed, tasks))
+    print(title[:len(title) - 3])
+    y.close()
---
+++ 0x0E-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""lists progress of employee's tasks through REST API"""
+
+if __name__ == "__main__":
+    import csv
+    import json
+    import sys
+    import urllib.request
+
+    user_id = sys.argv[1]
+    completed = 0
+    tasks = 0
+    rows = [[]]
+    x = urllib.request.urlopen('https://jsonplaceholder.typicode.com/todos/')
+    todo = json.loads(x.read())
+    for i in todo:
+        if i.get('userId') == int(user_id):
+            tasks += 1
+            y = urllib.request.urlopen('https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
+            user = json.loads(y.read())
+            status = str(i.get('completed'))
+            title = i.get('title')
+            username = user.get('username')
+            rows.append([user_id, username, status, title])
+            y.close()
+
+    x.close()
+
+    with open('{}.csv'.format(user_id), 'w') as f:
+        csvwriter = csv.writer(f, quoting=csv.QUOTE_ALL)
+       # csvwriter.writerow(fields)
+        csvwriter.writerows(rows)
