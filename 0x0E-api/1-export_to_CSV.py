182a014cb5c8fbf5de5db9edb9d29f09bb439912
Code Slayer <3613@holbertonschool.com>
2022-03-24 17:39:05 -0500

    changed to requests task 1

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -4,29 +4,24 @@
 if __name__ == "__main__":
     import csv
     import json
+    import requests
     import sys
-    import urllib.request
 
     user_id = sys.argv[1]
     completed = 0
     tasks = 0
     rows = [[]]
-    x = urllib.request.urlopen('https://jsonplaceholder.typicode.com/todos/')
-    todo = json.loads(x.read())
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos/').json()
     for i in todo:
         if i.get('userId') == int(user_id):
             tasks += 1
-            y = urllib.request.urlopen(
+            user = requests.get(
                 'https://jsonplaceholder.typicode.com/users/{}'
-                .format(user_id))
-            user = json.loads(y.read())
+                .format(user_id)).json()
             status = i.get('completed')
             title = i.get('title')
             username = user.get('username')
             rows.append([user_id, username, status, title])
-            y.close()
-
-    x.close()
 
     with open('{}.csv'.format(user_id), 'w') as f:
         csvwriter = csv.writer(f, quoting=csv.QUOTE_ALL)
