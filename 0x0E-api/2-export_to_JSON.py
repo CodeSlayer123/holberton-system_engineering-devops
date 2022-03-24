20742f0751fe12190e08aa1aa762e8b736a29c36
Code Slayer <3613@holbertonschool.com>
2022-03-24 17:41:56 -0500

    changed to requests task 2

--- 0x0E-api/2-export_to_JSON.py
+++ 0x0E-api/2-export_to_JSON.py
@@ -3,21 +3,18 @@
 
 if __name__ == "__main__":
     import json
+    import requests
     import sys
-    import urllib.request
 
     user_id = sys.argv[1]
     data = {}
     my_list = []
 
-    y = urllib.request.urlopen('https://jsonplaceholder.typicode.com/users/{}'
-                               .format(user_id))
-    user = json.loads(y.read())
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(user_id)).json()
     username = user.get('username')
-    y.close()
 
-    x = urllib.request.urlopen('https://jsonplaceholder.typicode.com/todos/')
-    todo = json.loads(x.read())
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos/').json()
     for i in todo:
         if i.get('userId') == int(user_id):
             tmp = {}
@@ -27,7 +24,6 @@ if __name__ == "__main__":
             tmp["completed"] = status
             tmp["username"] = username
             my_list.append(tmp)
-    x.close()
     data[user_id] = my_list
 
     with open('{}.json'.format(user_id), 'w') as f:
