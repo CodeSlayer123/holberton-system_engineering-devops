de2dafc745374d43ae5b9dfb0c51ea170ca0f612
Code Slayer <3613@holbertonschool.com>
2022-03-24 17:33:59 -0500

    json revised

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -8,23 +8,19 @@ if __name__ == "__main__":
     completed = 0
     tasks = 0
     title = "\t "
-    #x = urllib.request.urlopen('https://jsonplaceholder.typicode.com/todos/')
-    x = requests.get("https://jsonplaceholder.typicode.com/todos/")
-    todo = json.loads(x.read())
+
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
     for i in todo:
         if i.get('userId') == int(user_id):
             tasks += 1
             if i.get('completed') is True:
                 title = title + i['title'] + '\n' + '\t '
                 completed += 1
-    x.close()
 
     #y = urllib.request.urlopen('https://jsonplaceholder.typicode.com/users/{}'
     #                           .format(user_id))
-    y = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id))
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id)).json()
 
-    user = json.loads(y.read())
     print("Employee {} is done with tasks ({}/{}):"
           .format(user['name'], completed, tasks))
     print(title[:len(title) - 3])
-    y.close()
